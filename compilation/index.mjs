bc41108edd5dda8af2c5d228e9fa17c0aa5eaf2a
zimberzimber <levshakhgeldian@gmail.com>
2025-12-05 01:48:31 +0200

    Moved Navigation Tree Creation to Compilation Step

--- compilation/index.mjs
+++ compilation/index.mjs
@@ -2,6 +2,10 @@ import fs from "fs"
 import path from "path"
 import { execSync, spawnSync } from "child_process"
 import filenamify from 'filenamify';
+import * as cheerio from 'cheerio';
+
+const element = (html) => cheerio.load(html, {}, false)("*")
+
 
 // --skipcontent : Skips converting page content
 // --debugoutput : Output into two documents containing all nodes and backlins
@@ -10,6 +14,7 @@ import filenamify from 'filenamify';
 const IGNORED_DIRS = [".obsidian", "Assets"]
 const RESOURCE_DIR = "./resources"
 
+// TODO: Remove git dependency
 const rootDir = execSync("git rev-parse --show-toplevel", { encoding: "utf8" }).trim()
 
 const vaultDir = `${rootDir}/obsidian`
@@ -18,16 +23,6 @@ const outputDir = `${rootDir}/docs`
 const skipContent = process.argv.includes("--skipcontent")
 const debugOutput = process.argv.includes("--debugoutput")
 const localbackLinks = process.argv.includes("--localbacklinks")
-
-let backlinkPrefix = ""
-if (localbackLinks)
-    backlinkPrefix = `file://${outputDir}`
-else
-    backlinkPrefix = "/" + execSync("git config --get remote.origin.url", { encoding: "utf8" }).trim().split('/').pop().replace(/\.git$/g, '')
-
-const htmlTemplate = fs.readFileSync(RESOURCE_DIR + "/template.html", "utf8")
-    .replace("{{STATIC.SCRIPT}}", fs.readFileSync(RESOURCE_DIR + "/script.js", "utf8"))
-    .replace("{{STATIC.STYLE}}", fs.readFileSync(RESOURCE_DIR + "/style.css", "utf8"))
 
 const assetsDir = `${vaultDir}/Assets`
 
@@ -125,20 +120,6 @@ function processSortspecEntry(filePath) {
     return content
 }
 
-function postProcessContent(nodes, navData) {
-    for (const node of nodes) {
-        if (node.content) {
-            node.content = htmlTemplate
-                .replaceAll("{{PAGE.TITLE}}", node.title)
-                .replace("{{PAGE.NAV_DATA}}", "const BACKLINK_NAV_DATA = " + JSON.stringify(navData))
-                .replace("{{PAGE.CONTENT}}", node.content)
-        }
-
-        if (node.children)
-            postProcessContent(node.children, navData)
-    }
-}
-
 function outputNodes(nodes, dir) {
     for (const node of nodes) {
         if (node.content) {
@@ -156,34 +137,82 @@ function outputNodes(nodes, dir) {
 }
 
 function buildNavigationHierarchy(nodes, relativePath) {
-    const list = []
+    nodes = nodes.filter(n => n.content || (n.children && n.children.length > 0))
+    if (nodes.length == 0)
+        return null
+
+    const ul = element('<ul class="navtree"></ul>')
 
     for (const node of nodes) {
-        if (!node.content && !node.children)
-            continue
+        const li = element('<li class="navtree"></li>')
 
-        const link = {
-            title: node.title,
-            path: null,
-            children: null
+        let label
+        if (node.content) {
+            label = element(`<a class="navtree"></a>`)
+            label.attr("href", `${relativePath}/${node.path_part}.html`)
+        }
+        else
+            label = element(`<span class="navtree"></span>`)
+
+        label.text(node.title)
+        label.attr("nav-title", node.title)
+        li.append(label)
+
+        if (node.children) {
+            const children = buildNavigationHierarchy(node.children, `${relativePath}/${node.path_part}`)
+            li.append(children)
         }
 
-        if (node.content)
-            link.path = `${relativePath}/${node.path_part}.html`
-
-        if (node.children)
-            link.children = buildNavigationHierarchy(node.children, `${relativePath}/${node.path_part}`)
-
-        list.push(link)
+        ul.append(li)
     }
 
-    return list
+    return ul
+}
+
+function buildHtmlTemplate(nodes) {
+    let backlinkPrefix
+    if (localbackLinks)
+        backlinkPrefix = `file://${outputDir}`
+    else
+        backlinkPrefix = "/" + execSync("git config --get remote.origin.url", { encoding: "utf8" }).trim().split('/').pop().replace(/\.git$/g, '')
+    // TODO: Remove git dependency
+
+    const navHierarchy = buildNavigationHierarchy(nodes, backlinkPrefix)
+
+    const template = cheerio.load(fs.readFileSync(RESOURCE_DIR + "/template.html", "utf8"))
+    template("#custom_script").text(fs.readFileSync(RESOURCE_DIR + "/script.js", "utf8"))
+    template("#custom_style").text(fs.readFileSync(RESOURCE_DIR + "/style.css", "utf8"))
+    template("#navigation_tree").append(navHierarchy)
+    return template
+}
+
+function processToPages(nodes, htmlTemplate) {
+    for (const node of nodes) {
+        if (node.content) {
+            const html = cheerio.load(htmlTemplate.html())
+            html("title").text(node.title)
+            html("#page_title").text(node.title)
+
+            const t = node.title.replaceAll(/'/g, "\\'")
+            const selector = `a.navtree[nav-title='${t}'], span.navtree[nav-title='${t}']`
+            try {
+                html(selector).attr("selected", true)
+            }
+            catch (e){
+                console.log(e)
+            }
+            html(`#page_content`).append(node.content)
+            node.content = html.html()
+        }
+
+        if (node.children)
+            processToPages(node.children, htmlTemplate)
+    }
 }
 
 const contentNodes = gather(vaultDir)
-
-const navHierarchy = buildNavigationHierarchy(contentNodes, backlinkPrefix)
-postProcessContent(contentNodes, navHierarchy)
+const htmlTemplate = buildHtmlTemplate(contentNodes)
+processToPages(contentNodes, htmlTemplate)
 
 if (debugOutput) {
     fs.writeFileSync(outputDir + "/nodes.json", JSON.stringify(contentNodes, null, 2))
--- compilation/resources/script.js
+++ compilation/resources/script.js
@@ -1,58 +1,4 @@
 
-
-function normalizeWikilinks() {
-	const openNodes = [...BACKLINK_NAV_DATA]
-	const linkDictionary = {}
-	while (openNodes.length > 0) {
-		node = openNodes.pop()
-
-		if (node.path)
-			linkDictionary[node.title] = node.path
-
-		if (node.children)
-			openNodes.push(...node.children)
-	}
-
-	for (const wikilink of document.querySelectorAll("a.wikilink")) {
-		const fixedLink = linkDictionary[wikilink.innerText]
-		if (fixedLink)
-			wikilink.href = fixedLink
-	}
-}
-
-function buildNavigationTree(data) {
-	const ul = document.createElement('ul')
-	ul.classList.add("navtree")
-
-	for (const node of data) {
-		const li = document.createElement('li')
-		li.classList.add("navtree")
-
-		if (node.path) {
-			const a = document.createElement('a')
-			a.classList.add("navtree")
-
-			a.textContent = node.title;
-			a.href = node.path;
-			li.appendChild(a);
-		} else {
-			const span = document.createElement('span')
-			span.classList.add("navtree")
-
-			span.textContent = node.title;
-			li.appendChild(span)
-		}
-
-		if (node.children)
-			li.appendChild(buildNavigationTree(node.children))
-
-		ul.appendChild(li)
-	}
-
-	return ul;
-}
-
-
 function addCodeBlockCopyButtons() {
 	const codeBlocks = document.querySelectorAll("div.sourceCode")
 	for (block of codeBlocks) {
@@ -80,9 +26,5 @@ function copyCodeBlock(caller) {
 
 window.onload = () => {
 	hljs.highlightAll()
-	normalizeWikilinks()
 	addCodeBlockCopyButtons()
-
-	const xd = buildNavigationTree(BACKLINK_NAV_DATA)
-	document.querySelector(".side").appendChild(xd)
 }
